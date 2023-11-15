a3236e7b6bfac195f009a11e05781efffcee610b
ravi4246 <129968324+ravi4246@users.noreply.github.com>
2023-11-15 14:38:41 +0530

    Add files via upload

---
+++ canny edge.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+kernel=np.ones((5,5),np.uint8)
+print(kernel)
+path="arch.jpg"
+img=cv2.imread(path)
+imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgGaussianblur=cv2.GaussianBlur(img,(7,7),0)
+imgCanny=cv2.Canny(imgGaussianblur,100,200)
+cv2.imshow("canny",imgCanny)
+cv2.waitKey(0)
